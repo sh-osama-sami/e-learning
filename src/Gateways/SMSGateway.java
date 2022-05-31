de3a4c168fe233bcb502575009a436dbb52293f5
ibrahimsoltan <70820767+ibrahimsoltan@users.noreply.github.com>
2022-05-31 02:43:53 +0200

    some minor changes in gateways
    send implementation changed

--- src/Gateways/SMSGateway.java
+++ src/Gateways/SMSGateway.java
@@ -8,31 +8,7 @@ import Messages.TaskAddedMobileMessage;
 public class SMSGateway {
 	
 	public void sendMessage(Message message, String user,String content[]) {
-		String[] placeHolders = new String[] {}; // set some place holders here 
-		
-		if(message instanceof DailyNewsMobileMessage) {
-			DailyNewsMobileMessage msg = (DailyNewsMobileMessage) message;
-			
-			msg.prepareMessage(placeHolders);
-			
-			// some code to send message
-		}
-		
-		else if(message instanceof GradesAnnouncementMobileMessage) {
-			GradesAnnouncementMobileMessage msg = (GradesAnnouncementMobileMessage) message;
-			
-			msg.prepareMessage(placeHolders);
-			
-			// some code to send message
-		}
-		
-		else if(message instanceof TaskAddedMobileMessage) {
-			TaskAddedMobileMessage msg = (TaskAddedMobileMessage) message;
-			
-			msg.prepareMessage(placeHolders);
-			
-			// some code to send message to user
-		}
+		message.prepareMessage(content);
 		
 	}
 }
